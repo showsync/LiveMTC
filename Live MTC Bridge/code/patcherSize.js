2af4bea5c5d4ac0104ac992a7bb48d4b743c44bc
Jean-Paul Grobben <mail@soundsuite.nl>
2024-07-04 16:28:46 +0200

    patcherSize.js: Prettier cleanup

--- Live MTC Bridge/code/patcherSize.js
+++ Live MTC Bridge/code/patcherSize.js
@@ -1,69 +1,65 @@
-autowatch = 1;
+// autowatch = 1;
 outlets = 2;
 
 function listAreas() {
-	var areas = getAreas();
-	for (var i = 0; i < areas.length; i++) {
-		var area = areas[i];
-		outlet(0, area.areaName, area.rectangle);
-	}
+  var areas = getAreas();
+  for (var i = 0; i < areas.length; i++) {
+    var area = areas[i];
+    outlet(0, area.areaName, area.rectangle);
+  }
 }
 
 function show() {
-	var totalRectangle = null;
-	if (arguments.length > 0) {
-		var args = arrayfromargs(arguments);
-		for (var i = 0; i < args.length; i++) {
-			var areaName = args[i];
-			var areas = getAreas();
-			for (var j = 0; j < areas.length; j++) {
-				var area = areas[j];
-				if (area.areaName == areaName) {
-					if (totalRectangle) {
-						totalRectangle = merge(totalRectangle, area.rectangle);
-					}
-					else {
-						totalRectangle = area.rectangle;
-					}
-				}
-			}
-		}
-	}
-	if (totalRectangle) {
-		//post("set area to " + totalRectangle);
-		outlet(1, totalRectangle);
-	}
-	else {
-		post(arrayfromargs(arguments), "not part of known areas", "\n");
-	}
+  var totalRectangle = null;
+  if (arguments.length > 0) {
+    var args = arrayfromargs(arguments);
+    for (var i = 0; i < args.length; i++) {
+      var areaName = args[i];
+      var areas = getAreas();
+      for (var j = 0; j < areas.length; j++) {
+        var area = areas[j];
+        if (area.areaName == areaName) {
+          if (totalRectangle) {
+            totalRectangle = merge(totalRectangle, area.rectangle);
+          } else {
+            totalRectangle = area.rectangle;
+          }
+        }
+      }
+    }
+  }
+  if (totalRectangle) {
+    //post("set area to " + totalRectangle);
+    outlet(1, totalRectangle);
+  } else {
+    post(arrayfromargs(arguments), "not part of known areas", "\n");
+  }
 }
 
 function getAreas() {
-	var areas = new Array();
-    var prev = 0;
-	var objectToCheck = this.patcher.firstobject;
-	do {
-		if (objectToCheck.maxclass == "panel" && objectToCheck.varname != "") {
-			var area = new Object();
-			area.areaName = objectToCheck.varname;
-			area.rectangle = objectToCheck.rect;
-			areas.push(area);
-		}
-	}
-	while (objectToCheck = objectToCheck.nextobject)	
-	return areas;
+  var areas = new Array();
+  var prev = 0;
+  var objectToCheck = this.patcher.firstobject;
+  do {
+    if (objectToCheck.maxclass == "panel" && objectToCheck.varname != "") {
+      var area = new Object();
+      area.areaName = objectToCheck.varname;
+      area.rectangle = objectToCheck.rect;
+      areas.push(area);
+    }
+  } while ((objectToCheck = objectToCheck.nextobject));
+  return areas;
 }
 
 function merge(r1, r2) {
-   return [ Math.min(r1[0], r2[0]),
-            Math.min(r1[1], r2[1]),
-            Math.max(r1[2], r2[2]),
-            Math.max(r1[3], r2[3])]
+  return [
+    Math.min(r1[0], r2[0]),
+    Math.min(r1[1], r2[1]),
+    Math.max(r1[2], r2[2]),
+    Math.max(r1[3], r2[3]),
+  ];
 }
 
 function isEqual(r1, r2) {
-   return (r1[0] == r2[0] &&
-			r1[1] == r2[1] &&
-			r1[2] == r2[2] &&
-			r1[3] == r2[3])
+  return r1[0] == r2[0] && r1[1] == r2[1] && r1[2] == r2[2] && r1[3] == r2[3];
 }
